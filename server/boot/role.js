7f6543e487ce79b5869ac2752822ff28b300003f
Paulo McNally <paulomcnally@gmail.com>
2017-03-24 01:56:08 -0600

    add role and other changes

---
+++ server/boot/role.js
@@ -0,0 +1,39 @@
+module.exports = function roleInit(server) {
+  var Role = server.models.Role;
+  var RoleMapping = server.models.RoleMapping;
+
+  //insert row on model RoleMapping
+  function addRoleMappingAdmin(role, userId) {
+    RoleMapping.findOne({
+      where: {
+        'principalid': userId
+      }
+    }, function(err, res) {
+      if (res === null) {
+        role.principals.create({
+          principalType: RoleMapping.USER,
+          principalId: userId // user ID
+        }, function(err, principal) {});
+      }
+    });
+  }
+
+  Role.findOne({
+    where: {
+      'name': 'admin'
+    }
+  }, function(err, res) {
+    if (res === null) {
+      //create the admin role
+      Role.create({
+        name: 'admin'
+      }, function(err, role) {
+        //make bob an admin
+        addRoleMappingAdmin(role, 1);
+      });
+    }
+    else {
+      addRoleMappingAdmin(res, 1);
+    }
+  });
+};
